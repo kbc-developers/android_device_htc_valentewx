CyanogenMod/android_device_htc_ville

ville: Clean up makefile inheritance/ordering
* Clean up cm.mk
* Add full_ville.mk makefile
* Clean up device.mk

Change-Id: I8cc8325e6c8e50f2bce00419ba87706ff4718533

cm.mk
@@ -1,15 +1,7 @@
-# Release name
-PRODUCT_RELEASE_NAME := ville
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 960
-TARGET_SCREEN_WIDTH := 540
+$(call inherit-product, device/htc/ville/full_ville.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/htc/ville/device_ville.mk)
 
 # Device naming
 PRODUCT_DEVICE := ville
@@ -19,4 +11,8 @@ PRODUCT_MODEL := One S
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ville BUILD_FINGERPRINT=htc_europe/ville/ville:4.1.1/JRO03C/128506.8:user/release-keys PRIVATE_BUILD_DESC="3.16.401.8 CL128506 release-keys" BUILD_NUMBER=128506
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="ville" \
+    PRODUCT_NAME="ville" \
+    BUILD_FINGERPRINT="htc_europe/ville/ville:4.1.1/JRO03C/128506.8:user/release-keys" \
+    PRIVATE_BUILD_DESC="3.16.401.8 CL128506 release-keys"

kbc-developers/android_device_htc_valentewx

cm.mk
@@ -1,15 +1,7 @@
-# Release name
-PRODUCT_RELEASE_NAME := valentewx
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 960
-TARGET_SCREEN_WIDTH := 540
+$(call inherit-product, device/htc/ville/full_valentewx.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/htc/valentewx/device_valentewx.mk)
 
 # Device naming
 PRODUCT_DEVICE := valentewx
@@ -19,4 +11,8 @@ PRODUCT_MODEL := ISW13HT
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_valentewx BUILD_FINGERPRINT=htc_europe/valentewx/valentewx:4.1.1/JRO03C/128506.8:user/release-keys PRIVATE_BUILD_DESC="3.16.401.8 CL128506 release-keys" BUILD_NUMBER=128506
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="valentewx" \
+    PRODUCT_NAME="valentewx" \
+    BUILD_FINGERPRINT="htc_europe/valentewx/valentewx:4.1.1/JRO03C/128506.8:user/release-keys" \
+    PRIVATE_BUILD_DESC="3.16.401.8 CL128506 release-keys"
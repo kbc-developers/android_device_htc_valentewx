3c0d531a9c769ea1dca127cf3634ad2483f2b3f5
Ethan Chen <intervigil@gmail.com>
2016-05-05 22:21:39 -0700

    ville: Clean up makefile inheritance/ordering
    * Clean up cm.mk
    * Add full_ville.mk makefile
    * Clean up device.mk
    
    Change-Id: I8cc8325e6c8e50f2bce00419ba87706ff4718533

--- cm.mk
+++ cm.mk
@@ -1,15 +1,7 @@
-# Release name
-PRODUCT_RELEASE_NAME := valentewx
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 960
-TARGET_SCREEN_WIDTH := 540
+$(call inherit-product, device/htc/ville/full_valentewx.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/htc/valentewx/device_valentewx.mk)
 
 # Device naming
 PRODUCT_DEVICE := valentewx
@@ -19,4 +11,8 @@ PRODUCT_MODEL := ISW13HT
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_valentewx BUILD_FINGERPRINT=htc_europe/valentewx/valentewx:4.1.1/JRO03C/128506.8:user/release-keys PRIVATE_BUILD_DESC="3.16.401.8 CL128506 release-keys" BUILD_NUMBER=128506
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="valentewx" \
+    PRODUCT_NAME="valentewx" \
+    BUILD_FINGERPRINT="htc_europe/valentewx/valentewx:4.1.1/JRO03C/128506.8:user/release-keys" \
+    PRIVATE_BUILD_DESC="3.16.401.8 CL128506 release-keys"
