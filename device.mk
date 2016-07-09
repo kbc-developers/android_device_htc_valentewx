CyanogenMod/android_device_htc_ville

ville: Move legacy audio flags/props down from s4-common
Change-Id: Id630138c479963088f40eda4e806d7d98dbc0a14

device.mk
@@ -35,6 +35,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
+    device/htc/ville/configs/audio_policy.conf:system/etc/audio_policy.conf \
     device/htc/ville/dsp/snd_soc_msm/snd_soc_msm_2x:/system/etc/snd_soc_msm/snd_soc_msm_2x
 
 # FM radio

kbc-developers/android_device_htc_valentewx

device.mk
@@ -35,6 +35,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
+    device/htc/valentewx/configs/audio_policy.conf:system/etc/audio_policy.conf \
     device/htc/valentewx/dsp/snd_soc_msm/snd_soc_msm_2x:/system/etc/snd_soc_msm/snd_soc_msm_2x
 
 # FM radio